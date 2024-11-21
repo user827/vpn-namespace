91cae459d5b349c33b1a15ba5c0ebb50c74df540
user827 <user827@users.noreply.github.com>
2024-11-21 09:16:27 +0200

    Clean up

--- autogen.sh
+++ autogen.sh
@@ -11,29 +11,16 @@ if [ -f .git/hooks/pre-commit.sample ] && [ ! -f .git/hooks/pre-commit ]; then
     echo "Activated pre-commit hook." || :
 fi
 
-#intltoolize --force --automake
 autoreconf --verbose --force --install --symlink
-
-#libdir() {
-#  echo $(cd "$1/$(gcc -print-multi-os-directory)"; pwd)
-#}
 
 args="\
   --sysconfdir=/etc \
   --localstatedir=/var \
 "
-#--libdir=$(libdir /usr/lib) \
 
 if [ -f "$topdir/.config.args" ]; then
   args="$args $(cat "$topdir"/.config.args)"
 fi
-
-#if [ ! -L /bin ]; then
-#  args="$args \
-#    --with-rootprefix=/ \
-#    --with-rootlibdir=$(libdir /lib) \
-#    "
-#fi
 
 cd "$oldpwd"
 
